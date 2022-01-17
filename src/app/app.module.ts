6b08b28fd18965cad15c8122cbf65b0182a31354
Lucas <lucaspagoti95@gmail.com>
2022-01-17 13:27:00 -0300

    starting navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuizFormComponent } from './quiz-form/quiz-form.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuizFormComponent
+    QuizFormComponent,
+    NavigationBarComponent
   ],
   imports: [
     BrowserModule,
