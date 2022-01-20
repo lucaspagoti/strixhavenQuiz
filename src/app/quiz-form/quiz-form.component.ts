b2a2d3ca2f502344e6eca1235714a2a739a95b8d
lucaspagoti <lucaspagoti95@gmail.com>
2022-01-20 18:05:26 -0300

    quiz to ts

--- src/app/quiz-form/quiz-form.component.ts
+++ src/app/quiz-form/quiz-form.component.ts
@@ -7,8 +7,23 @@ import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
   templateUrl: './quiz-form.component.html',
   styleUrls: ['./quiz-form.component.scss']
 })
+
+
 export class QuizFormComponent implements OnInit {
   
+index: number = 0
+
+perguntas = [
+  { texto: "Qual a sua cor favorita?", respostas: [{texto: 'azul', peso: 1}, {texto: 'verde', peso: 2}], key: "corFavorita" }, 
+  { texto: "Você é corajoso?", respostas: [{texto: 'Sim', peso: 1}, {texto: 'Não', peso: 2}], key:"corajosa" },
+  { texto: "Você sabe TI?", respostas: [{texto: 'Sim', peso: 1}, {texto: 'Não', peso: 2}], key:"sabeTi" },
+  { texto: "Você é estudioso?", respostas: [{texto: 'Sim', peso: 1}, {texto: 'Não', peso: 2}], key:"estudiosa" },
+ ]
+
+continuar() {
+  this.index += 1
+}
+
   
   @Input() 
   pergunta!: string; 
@@ -19,12 +34,8 @@ export class QuizFormComponent implements OnInit {
   @Output() setPesoFormulario = new EventEmitter<{peso: number, key: string}>();
   constructor() { }
 
-  ngOnInit(): void {
-         
-   
-    
+  ngOnInit(): void {    
   }
-
 
   salvarPesoPergunta(peso: number) {
     this.setPesoFormulario.emit({key: this.key, peso: peso});
